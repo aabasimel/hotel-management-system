3f7dfe31557480d2e26b1d43735c9027b2dc1219
Ahmed <abasimelahmed@gmail.com>
2025-09-09 23:46:11 +0200

    Modified AuthController

--- src/main/java/com/ahmedabasimel/myhotel/controller/AuthController.java
+++ src/main/java/com/ahmedabasimel/myhotel/controller/AuthController.java
@@ -3,13 +3,20 @@ package com.ahmedabasimel.myhotel.controller;
 import com.ahmedabasimel.myhotel.exception.UserAlreadyExistsException;
 import com.ahmedabasimel.myhotel.models.User;
 import com.ahmedabasimel.myhotel.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
     private final UserService userService;
 
@@ -29,5 +36,8 @@ public class AuthController {
 
         }
     }
+
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody)
+
 
 }
